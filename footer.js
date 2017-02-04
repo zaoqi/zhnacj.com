7b61a277e96a2a41d01737a8caa2d56b4dff7124
zaoqi <zaoqiff@gmail.com>
2017-02-04 22:27:31 +0800

    增加footer

---
+++ footer.js
@@ -0,0 +1,35 @@
+//中南财经网
+//Copyright (C) 2017  zaoqi
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero General Public License for more details.
+
+//You should have received a copy of the GNU Affero General Public License
+//along with this program.  If not, see [http://www.gnu.org/licenses/].
+document.write(hereDoc(function(){/*
+<style type="text/css">
+
+footer {
+	width: 100%;
+	background-color: #000;
+	color: #FFF;
+	display: inline-block;
+	text-align: center;
+}
+
+footer a {
+	color: #FFF;
+}
+</style>
+<footer>
+<p>Copyright (C) 2017  zaoqi</p>
+<a href="https://github.com/zaoqi/zhnacj.com">这个网站在GitHub开发。</a>
+</footer>
+*/}));
--- header.js
+++ header.js
@@ -13,9 +13,6 @@
 
 //You should have received a copy of the GNU Affero General Public License
 //along with this program.  If not, see [http://www.gnu.org/licenses/].
-function hereDoc(func) {
-	return func.toString().split(/\n/).slice(1, -1).join('\n');
-}
 document.write(hereDoc(function(){/*
 <style type="text/css">
 * {
