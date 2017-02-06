e274f79652c2d8f6af02cce3817e62744f70c7c2
zaoqi <zaoqiff@gmail.com>
2017-02-06 16:03:39 +0800

    减少重复代码

--- lib.js
+++ lib.js
@@ -16,6 +16,20 @@
 function hereDoc(func) {
 	return func.toString().split(/\n/).slice(1, -1).join('\n');
 }
+function download(url) {
+	var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.open("GET",url,false);
+xmlhttp.send();
+return xmlhttp.responseText;
+}
 document.write(hereDoc(function(){/*
 <style type="text/css">
 * {
